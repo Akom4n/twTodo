3fbc66b397d2bb254eb79805c0084cd7e855e415
Vinicius Silva <xdenik13@gmail.com>
2024-06-09 12:34:55 -0300

    Configuração e teste de injeção de dados

--- src/main/java/com/den/tasklist/controllers/HomeController.java
+++ src/main/java/com/den/tasklist/controllers/HomeController.java
@@ -1,13 +1,33 @@
 package com.den.tasklist.controllers;
 
+import com.den.tasklist.repositories.TodoRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    private final TodoRepository todoRepository;
+
+    public HomeController(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
     @GetMapping("/")
-    public String home(){
-        return "home";
+    public ModelAndView home(){
+        var modelAndView = new ModelAndView("home");
+        modelAndView.addObject("nome", "Vinícius Silva");
+        var alunos = List.of("Vinicius Silva", "Leticia Lima", "João Vinícius");
+        modelAndView.addObject("alunos", alunos);
+        modelAndView.addObject("ehProgramador", false);
+
+
+        var todos = todoRepository.findAll();
+        System.out.println(todos);
+
+        return modelAndView;
     }
 }
